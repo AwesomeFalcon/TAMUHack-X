63317f719e9e7b1e90ddf576b4fd190197ee25f2
AwesomeFalcon <aaryap251@gmail.com>
2024-01-28 01:52:48 -0600

    Add files via upload

--- popup.js
+++ popup.js
@@ -6,6 +6,13 @@ document.getElementById("scrapeEmails").addEventListener("click", async () => {
     });
 })
 
-function scrapeEmailsFromPage() {
-    
+async function scrapeEmailsFromPage() {
+    const html = document.documentElement.outerHTML;
+    const doc = new DOMParser().parseFromString(html, 'text/html');
+
+    const paragraphs = doc.querySelectorAll('p'); 
+    const extractedText = Array.from(paragraphs).map(p => p.textContent.trim()).join('\n');
+    alert("hi");
+
+    return extractedText; 
 }
