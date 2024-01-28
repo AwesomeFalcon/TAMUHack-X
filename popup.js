9faf576ede82da2b8afc6c78b81dae37ced58026
AwesomeFalcon <aaryap251@gmail.com>
2024-01-28 11:20:27 -0600

    Add files via upload

--- popup.js
+++ popup.js
@@ -2,10 +2,28 @@ document.getElementById("scrapeEmails").addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({active: true, currentWindow: true})
 
     chrome.scripting.executeScript({
-        target: {tabId: tab.id}, func: scrapeEmailsFromPage()
+        target: {tabId: tab.id}, func: scrapeEmailsFromPage
     });
 })
 
-function scrapeEmailsFromPage() {
-    
+document.addEventListener('DOMContentLoaded', function() {
+    var run = document.getElementById('scrapeEmails');
+    var change = document.getElementById('results')
+    run.addEventListener('click', function() {
+        change.innerText = 'Worked!';
+    });
+});
+
+async function scrapeEmailsFromPage() {
+    const html = document.documentElement.outerHTML;
+    const doc = new DOMParser().parseFromString(html, 'text/html');
+
+    const paragraphs = doc.querySelectorAll('p'); 
+    let extractedText = '';
+    let count = 0;
+    paragraphs.forEach(paragraph => {
+        extractedText += paragraph.textContent.trim() + '\n'; 
+    });
+    alert(extractedText);
 }
+
