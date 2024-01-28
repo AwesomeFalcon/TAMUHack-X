58fed165995dce5f8096ba9fdc52d2e914fd3cd4
AwesomeFalcon <aaryap251@gmail.com>
2024-01-28 09:19:17 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,10 +2,19 @@ document.getElementById("scrapeEmails").addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({active: true, currentWindow: true})
 
     chrome.scripting.executeScript({
-        target: {tabId: tab.id}, func: scrapeEmailsFromPage()
+        target: {tabId: tab.id}, func: scrapeEmailsFromPage
     });
 })
 
-function scrapeEmailsFromPage() {
-    
+async function scrapeEmailsFromPage() {
+    const html = document.documentElement.outerHTML;
+    const doc = new DOMParser().parseFromString(html, 'text/html');
+
+    const paragraphs = doc.querySelectorAll('p'); 
+    let extractedText = '';
+    let count = 0;
+    paragraphs.forEach(paragraph => {
+        extractedText += paragraph.textContent.trim() + '\n'; 
+    });
+    alert(extractedText);
 }
